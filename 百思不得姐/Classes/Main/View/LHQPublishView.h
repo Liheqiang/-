f0e60b1f33f5f5dc96b328e67c04c4b45a04871b
HeqLee <359614874@qq.com>
2016-03-03 18:11:23 +0800

    新增发布界面
    之前的怎么不在了？我也不明白，我明明完成了，也提交了啊！

---
+++ 百思不得姐/Classes/Main/View/LHQPublishView.h
@@ -0,0 +1,16 @@
+//
+//  LHQPublishView.h
+//  百思不得姐
+//
+//  Created by HqLee on 16/3/3.
+//  Copyright © 2016年 HqLee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LHQPublishView : UIView
+
++ (instancetype)publishView;
++ (void)show;
+
+@end
---
+++ 百思不得姐/Classes/Main/View/LHQPublishView.m
@@ -0,0 +1,15 @@
+//
+//  LHQPublishView.m
+//  百思不得姐
+//
+//  Created by HqLee on 16/3/3.
+//  Copyright © 2016年 HqLee. All rights reserved.
+//
+
+#import "LHQPublishView.h"
+
+@implementation LHQPublishView
+
+
+
+@end
