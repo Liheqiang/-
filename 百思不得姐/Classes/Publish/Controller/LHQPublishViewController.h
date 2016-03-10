ea64aaac6d560f82a83ea1ab1568e8eba69cb677
HeqLee <359614874@qq.com>
2016-03-10 13:27:40 +0800

    新增发送段子

---
+++ 百思不得姐/Classes/Publish/Controller/LHQPublishViewController.h
@@ -0,0 +1,13 @@
+//
+//  LHQPublishViewController.h
+//  百思不得姐
+//
+//  Created by HqLee on 16/3/10.
+//  Copyright © 2016年 HqLee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LHQPublishViewController : UIViewController
+
+@end
---
+++ 百思不得姐/Classes/Publish/Controller/LHQPublishViewController.m
@@ -0,0 +1,22 @@
+//
+//  LHQPublishViewController.m
+//  百思不得姐
+//
+//  Created by HqLee on 16/3/10.
+//  Copyright © 2016年 HqLee. All rights reserved.
+//
+
+#import "LHQPublishViewController.h"
+
+@interface LHQPublishViewController ()
+
+@end
+
+@implementation LHQPublishViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+@end
